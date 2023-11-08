0da2c76fd7f518bbc18110127c04c5d55b524cf6
Tan-Aki <tanneguy.jullin@gmail.com>
2023-11-08 05:59:28 -0500

    chapter 13 : error handling

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,13 +1,14 @@
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
+import { notFound } from 'next/navigation';
 
 export default async function Page({ params }: { params: { id: string } }) {
     const id = params.id;
     const [invoice, customers] = await Promise.all([fetchInvoiceById(id), fetchCustomers()]);
 
     // Added by tan because TS was whining that the Form expect invoice props
-    if (!invoice) return <>something went wrong while fetching the invoice</>;
+    if (!invoice) notFound();
 
     return (
         <main>
