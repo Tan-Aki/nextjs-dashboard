6c31a34c8f2570313d1ec619ab76c1f9219c5a57
Tan-Aki <tanneguy.jullin@gmail.com>
2023-11-09 10:54:32 -0500

    chapter 16: metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,17 @@
 import '@/app/ui/global.css';
 
 import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
 
+export const metadata: Metadata = {
+    title: {
+        template: '%s | Acme Dashboard',
+        default: 'Acme Dashboard',
+    },
+    description: 'The official Next.js Course Dashboard, built with App Router.',
+    // https://nextjs.org/docs/app/api-reference/functions/generate-metadata#metadatabase
+    metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang='en'>
