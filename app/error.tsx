5d3cd9195f8af2de8004c99e48de3126a08632c7
Tan-Aki <tanneguy.jullin@gmail.com>
2023-12-20 19:03:00 -0500

    Add some error handling

---
+++ app/error.tsx
@@ -0,0 +1,37 @@
+'use client';
+
+import { useEffect } from 'react';
+
+// Handling errors for all nested pages unless they have their own error.js/not-found.js
+export default function Error({ error, reset }: { error: Error & { digest?: string }; reset: () => void }) {
+    useEffect(() => {
+        // Optionally log the error to an error reporting service
+        console.dir(error);
+        console.log(error.digest);
+    }, [error]);
+
+    // If not not-found file.js is present, you can handle it here instead
+
+    if (error.digest === 'NEXT_NOT_FOUND') {
+        return (
+            <main className='flex h-screen flex-col items-center justify-center'>
+                <h2 className='text-center'>Not found</h2>
+            </main>
+        );
+    }
+
+    return (
+        <main className='flex h-screen flex-col items-center justify-center'>
+            <h2 className='text-center'>Something went wrong!</h2>
+            <button
+                className='mt-4 rounded-md bg-blue-500 px-4 py-2 text-sm text-white transition-colors hover:bg-blue-400'
+                onClick={
+                    // Attempt to recover by trying to re-render the invoices route
+                    () => reset()
+                }
+            >
+                Try again
+            </button>
+        </main>
+    );
+}
